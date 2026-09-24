62baa7d37de8c519981676ffb5ed432674365872
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2026-09-24 11:17:29 -0300

    feat: setup a new router

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router";
 import { Home } from "@/features/home/home";
+import { Profile } from "@/features/profile/profile";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
   },
 ]);
